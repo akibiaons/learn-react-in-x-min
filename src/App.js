78d50273ed4652efe6c446b464135baeb6fa3648
Arthur Fernandez <arthur@snooget.com>
2023-05-20 09:25:01 -0700

    created a component and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ToDoList from "./ToDoList";
 
 function App() {
   return (
---
+++ src/ToDoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ToDoList() {
+  return (
+    <div>Hello World</div> // Will be adding more text but this will show up in <App>
+  );
+}
