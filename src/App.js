e369e8d9d21649c51ca688796e125747f6d2a477
Arthur Fernandez <arthur@snooget.com>
2023-05-20 09:36:48 -0700

    used  fragment to return jsx tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import ToDoList from "./ToDoList";
 
 function App() {
   return (
-    <ToDoList /> // This is JSX, Reacts version of html which allows for nested compnents
+    <>
+      <ToDoList />
+      <input type="text" />
+    </>
   );
 }
 
